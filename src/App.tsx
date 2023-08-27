85a3fabc3e70f7f17d8a4800ebe7fbbf764a2a27
ganeshpatil386386 <ganeshpatil386386@gmail.com>
2023-08-27 17:33:37 +0530

    TTT Game React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Block from './components/Block';
+import { stat } from 'fs';
 
 function App() {
+
+  const [state, setState] = useState(Array(9).fill(null));  
+  const [currentTurn, setCurrentTurn] = useState("X")
+
+  const checkWinner = (state: any[]) => {
+    const win = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,4,8],
+      [2,4,6],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8]
+    ];
+    for(let i=0; i < win.length; i++ ){
+      const [a,b,c] = win[i]
+      if(state[a] !== null && state[a] === state[b] && state[a] === state[c]) return true;
+
+    }
+    return false;
+  };
+
+  const handcleBlockClick = (index:number) => {
+
+    
+    const stateCopy = Array.from(state);
+    if(stateCopy[index] !== null) return;
+    stateCopy[index] = currentTurn;
+    setState(stateCopy);
+
+
+    const win = checkWinner(stateCopy);
+    if (win){
+      alert(`${currentTurn} won the game`)
+    }
+
+    setCurrentTurn(currentTurn === 'X' ? "O" : "X");
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      <div className='row'> 
+        <Block onClick={() => handcleBlockClick(0)} value={state[0]} />
+        <Block onClick={() => handcleBlockClick(1)} value={state[1]} />
+        <Block onClick={() => handcleBlockClick(2)} value={state[2]} />
+      </div>
+
+      <div className='row'> 
+        <Block onClick={() => handcleBlockClick(3)} value={state[3]}/>
+        <Block onClick={() => handcleBlockClick(4)} value={state[4]}/>
+        <Block onClick={() => handcleBlockClick(5)} value={state[5]}/>
+      </div>
+
+      <div className='row'> 
+        <Block onClick={() => handcleBlockClick(6)} value={state[7]}/>
+        <Block onClick={() => handcleBlockClick(7)} value={state[8]}/>
+        <Block onClick={() => handcleBlockClick(8)} value={state[6]}/>
+      </div>
     </div>
+    
   );
 }
 
